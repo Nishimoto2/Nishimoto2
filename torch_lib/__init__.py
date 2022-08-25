b157284dd6e63e09a268520293f651dcd832aca2
Nishimoto2 <111938562+Nishimoto2@users.noreply.github.com>
2022-08-25 13:08:02 +0900

    Update __init__.py

--- torch_lib/__init__.py
+++ torch_lib/__init__.py
@@ -1,6 +1,14 @@
+# 共通関数定義
+# Apache License Version 2.0
+# https://www.apache.org/licenses/LICENSE-2.0.html
+
+# ReadMe
+README = 'Common Library for PyTorch\nAuthor: M. Akaishi'
+
 import numpy as np
 import matplotlib.pyplot as plt
 import japanize_matplotlib
+
 import torch
 from torch import tensor
 import torch.nn as nn
@@ -29,8 +37,9 @@ def eval_loss(loader, device, net, criterion):
     loss = criterion(outputs, labels)
 
     return loss
- 
+  
 
+# 学習用関数
 def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device, history):
 
     base_epochs = len(history)
@@ -75,6 +84,8 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
             # 損失と精度の計算
             avg_train_loss = train_loss / count
             avg_train_acc = train_acc / count
+
+        #予測フェーズ
         net.eval()
         count = 0
 
@@ -105,8 +116,11 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
         item = np.array([epoch+1, avg_train_loss, avg_train_acc, avg_val_loss, avg_val_acc])
         history = np.vstack((history, item))
     return history
-  
-  def evaluate_history(history):
+
+
+
+# 学習ログ解析
+def evaluate_history(history):
   #損失と精度の確認
   print(f'初期状態: 損失: {history[0,3]:.5f} 精度: {history[0,4]:.5f}') 
   print(f'最終状態: 損失: {history[-1,3]:.5f} 精度: {history[-1,4]:.5f}' )
@@ -138,3 +152,60 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
   plt.title('学習曲線(精度)')
   plt.legend()
   plt.show()
+
+
+# イメージとラベル表示
+def show_images_labels(loader, classes, net, device):
+
+    # DataLoaderから最初の1セットを取得する
+    for images, labels in loader:
+        break
+    # 表示数は50個とバッチサイズのうち小さい方
+    n_size = min(len(images), 50)
+
+    if net is not None:
+      # デバイスの割り当て
+      inputs = images.to(device)
+      labels = labels.to(device)
+
+      # 予測計算
+      outputs = net(inputs)
+      predicted = torch.max(outputs,1)[1]
+      #images = images.to('cpu')
+
+    # 最初のn_size個の表示
+    plt.figure(figsize=(20, 15))
+    for i in range(n_size):
+        ax = plt.subplot(5, 10, i + 1)
+        label_name = classes[labels[i]]
+        # netがNoneでない場合は、予測結果もタイトルに表示する
+        if net is not None:
+          predicted_name = classes[predicted[i]]
+          # 正解かどうかで色分けをする
+          if label_name == predicted_name:
+            c = 'k'
+          else:
+            c = 'b'
+          ax.set_title(label_name + ':' + predicted_name, c=c, fontsize=20)
+        # netがNoneの場合は、正解ラベルのみ表示
+        else:
+          ax.set_title(label_name, fontsize=20)
+        # TensorをNumPyに変換
+        image_np = images[i].numpy().copy()
+        # 軸の順番変更 (channel, row, column) -> (row, column, channel)
+        img = np.transpose(image_np, (1, 2, 0))
+        # 値の範囲を[-1, 1] -> [0, 1]に戻す
+        img = (img + 1)/2
+        # 結果表示
+        plt.imshow(img)
+        ax.set_axis_off()
+    plt.show()
+
+
+# PyTorch乱数固定用
+
+def torch_seed(seed=123):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms = True
