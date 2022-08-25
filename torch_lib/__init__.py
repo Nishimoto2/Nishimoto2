05681d7841b0b9662546d1376e575f9fa5a04510
Nishimoto2 <111938562+Nishimoto2@users.noreply.github.com>
2022-08-25 12:40:51 +0900

    Update __init__.py

--- torch_lib/__init__.py
+++ torch_lib/__init__.py
@@ -1,1 +1,140 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import torch
+from torch import tensor
+import torch.nn as nn
+import torch.optim as optim
+from torchviz import make_dot
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as datasets
+from tqdm.notebook import tqdm
 
+# 損失関数値計算用
+def eval_loss(loader, device, net, criterion):
+  
+    # DataLoaderから最初の1セットを取得する
+    for images, labels in loader:
+        break
+
+    # デバイスの割り当て
+    inputs = images.to(device)
+    labels = labels.to(device)
+
+    # 予測値の計算
+    outputs = net(inputs)
+
+    #  損失値の計算
+    loss = criterion(outputs, labels)
+
+    return loss
+ 
+
+def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device, history):
+
+    base_epochs = len(history)
+  
+    for epoch in range(base_epochs, num_epochs+base_epochs):
+        train_loss = 0
+        train_acc = 0
+        val_loss = 0
+        val_acc = 0
+
+        #訓練フェーズ
+        net.train()
+        count = 0
+
+        for inputs, labels in tqdm(train_loader):
+            count += len(labels)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # 勾配の初期化
+            optimizer.zero_grad()
+
+            # 予測計算
+            outputs = net(inputs)
+
+            # 損失計算
+            loss = criterion(outputs, labels)
+            train_loss += loss.item()
+
+            # 勾配計算
+            loss.backward()
+
+            # パラメータ修正
+            optimizer.step()
+
+            # 予測値算出
+            predicted = torch.max(outputs, 1)[1]
+
+            # 正解件数算出
+            train_acc += (predicted == labels).sum().item()
+
+            # 損失と精度の計算
+            avg_train_loss = train_loss / count
+            avg_train_acc = train_acc / count
+    net.eval()
+        count = 0
+
+        for inputs, labels in test_loader:
+            count += len(labels)
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # 予測計算
+            outputs = net(inputs)
+
+            # 損失計算
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+
+            #予測値算出
+            predicted = torch.max(outputs, 1)[1]
+
+            #正解件数算出
+            val_acc += (predicted == labels).sum().item()
+
+            # 損失と精度の計算
+            avg_val_loss = val_loss / count
+            avg_val_acc = val_acc / count
+    
+        print (f'Epoch [{(epoch+1)}/{num_epochs+base_epochs}], loss: {avg_train_loss:.5f} acc: {avg_train_acc:.5f} val_loss: {avg_val_loss:.5f}, val_acc: {avg_val_acc:.5f}')
+        item = np.array([epoch+1, avg_train_loss, avg_train_acc, avg_val_loss, avg_val_acc])
+        history = np.vstack((history, item))
+    return history
+  
+  def evaluate_history(history):
+  #損失と精度の確認
+  print(f'初期状態: 損失: {history[0,3]:.5f} 精度: {history[0,4]:.5f}') 
+  print(f'最終状態: 損失: {history[-1,3]:.5f} 精度: {history[-1,4]:.5f}' )
+
+  num_epochs = len(history)
+  if num_epochs < 10:
+    unit = 1
+  else:
+    unit = num_epochs / 10
+
+  # 学習曲線の表示 (損失)
+  plt.figure(figsize=(9,8))
+  plt.plot(history[:,0], history[:,1], 'b', label='訓練')
+  plt.plot(history[:,0], history[:,3], 'k', label='検証')
+  plt.xticks(np.arange(0,num_epochs+1, unit))
+  plt.xlabel('繰り返し回数')
+  plt.ylabel('損失')
+  plt.title('学習曲線(損失)')
+  plt.legend()
+  plt.show()
+
+  # 学習曲線の表示 (精度)
+  plt.figure(figsize=(9,8))
+  plt.plot(history[:,0], history[:,2], 'b', label='訓練')
+  plt.plot(history[:,0], history[:,4], 'k', label='検証')
+  plt.xticks(np.arange(0,num_epochs+1,unit))
+  plt.xlabel('繰り返し回数')
+  plt.ylabel('精度')
+  plt.title('学習曲線(精度)')
+  plt.legend()
+  plt.show()
