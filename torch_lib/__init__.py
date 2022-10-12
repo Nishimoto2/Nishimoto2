1002e27e029a525cd848cfc6626495220cb12f53
Nishimoto2 <111938562+Nishimoto2@users.noreply.github.com>
2022-10-12 23:54:44 +0900

    Update __init__.py

--- torch_lib/__init__.py
+++ torch_lib/__init__.py
@@ -385,7 +385,7 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
                 outputs2 = net(img)
               
               print(outputs2)
-              print(outputs[i,:]
+              print(outputs[i,:])
               
               
               
