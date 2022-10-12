6f1663434b62f5d48e9bbe5434c42450131e5e03
Nishimoto2 <111938562+Nishimoto2@users.noreply.github.com>
2022-10-13 00:03:25 +0900

    Update __init__.py

--- torch_lib/__init__.py
+++ torch_lib/__init__.py
@@ -12,6 +12,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 import torchvision.datasets as datasets
 from tqdm.notebook import tqdm
+from PIL import Image
 
 # 損失関数値計算用
 def eval_loss(loader, device, net, criterion):
